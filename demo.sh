e32e704ec47730555622dc9cd3cab1063e73000e
a2802277 <30830492+a2802277@users.noreply.github.com>
2019-01-20 10:29:41 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,8 +1,18 @@
 if [ -d $1 ]; then
   echo "$1已经存在了"
-  exit
-else 
+exit
+else
+  mkdir $1
+  cd $1
   mkdir css js
-  touch index.html css/style.css js/main.js
-  exit
-  fi
+  
+  echo "<!DOCTYPE>
+ <title>Hello</title>
+ <h1>Hi</h1>" > index.html 
+
+  echo "h1{color: red;}" > css/style.css 
+  
+  echo "var string = "Hello World"
+ alert(string)" > js/main.js
+exit
+fi
